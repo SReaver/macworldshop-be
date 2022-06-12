8a1221d2ffb899a4359599308913e9a7937ddbb6
Sergey Plotnikov <reaverz@yandex.ru>
2022-06-13 00:26:22 +0600

    Add task 3.1

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,18 +1,42 @@
 'use strict';
+const productsData = require('./data.json')
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const getProductsList = async () => {
+	let body = '[]'
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+	try {
+		body = JSON.stringify(productsData)
+	} catch (e) { }
+
+	return {
+		statusCode: 200,
+		body
+	};
+}
+
+const getProductsById = async productId => {
+	// const productId = 1
+	console.log('productId', productId)
+	let body = '{}'
+	const filteredProducts = productsData.filter(el => el.id === productId)
+	// console.log(filteredProducts.length)
+	if (filteredProducts.length !== 0) {
+		try {
+			// console.log('FP', filteredProducts)
+			body = JSON.stringify(filteredProducts)
+			// console.log(body)
+		} catch (error) {
+			console.log(error.message)
+		}
+	}
+	return {
+		statusCode: 200,
+		body
+	}
+}
+
+getProductsById('1')//.then(data => { console.log(data) })
+module.exports = {
+	getProductsList,
+	getProductsById
+}
